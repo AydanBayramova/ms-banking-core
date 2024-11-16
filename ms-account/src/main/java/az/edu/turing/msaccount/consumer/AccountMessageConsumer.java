e898adf1417e17e5b6e9c7ea8a0f346c25819d2c
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-11-16 20:12:31 +0400

    create consul module and add dependency and properties

---
+++ ms-account/src/main/java/az/edu/turing/msaccount/consumer/AccountMessageConsumer.java
@@ -0,0 +1,15 @@
+package az.edu.turing.msaccount.consumer;
+
+import az.edu.turing.msaccount.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountMessageConsumer {
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
+    public void handleTransferMessage(String message) {
+        System.out.println("Received transfer event: " + message);
+        // Process the transfer event
+    }
+}
