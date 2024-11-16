ece9babe39cf9675fe245faaf5ce09527cc96236
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-11-16 20:14:04 +0400

    Add rabbitMQ dependency and create consumer and producer class for use rabbit

--- ms-account/src/main/java/az/edu/turing/msaccount/MsAccountApplication.java
+++ ms-account/src/main/java/az/edu/turing/msaccount/MsAccountApplication.java
@@ -2,8 +2,10 @@ package az.edu.turing.msaccount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsAccountApplication {
 
     public static void main(String[] args) {
